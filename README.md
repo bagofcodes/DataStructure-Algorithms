ce372cf33ac8388a91c225a0cbe022236a86d0d3
ANSHUMALI SHAW <37397899+ankugolu@users.noreply.github.com>
2020-07-30 10:10:58 +0530

    D1 Basics Of C++

---
+++ README.md
@@ -0,0 +1,5 @@
+# 30DaysOfCode
+
+## Day1-
+
+### Learned the basics of C++ programming and did some few basic programs using C++
---
+++ c++Basics/areaofcircle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int radius;
+    double area;
+    cout<<"Enter the radius: ";
+    cin>>radius;
+    area=3.14159*radius*radius;
+    cout<<"The Area of the circle is "<<area;
+    return 0;
+
+}
